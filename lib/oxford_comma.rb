f48fce5db90f64fe316d68d83c065f2a54b25bcb
Crystal Norman <cnorm88@gmail.com>
2020-09-18 16:05:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,14 @@
 def oxford_comma(array)
+  
 	string = ''
+	
 		if array.size == 1
 			return array[0]
 		end
 		if array.size == 2
 			return "#{array[0]} and #{array[1]}"
 		end  
+		
 	array.each do |word|
 		if word == array.last
 			string << "and #{word}"
